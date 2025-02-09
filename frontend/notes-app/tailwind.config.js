7f3a2f9af9edaac184b13314aefc59618f986378
abilash <abilashnarayanan2001@gmail.com>
2025-02-09 17:37:38 +0530

    login page ui completed

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      //colors used in entire proj
+      colors: {
+        primary : "#2B85FF",
+        secondary : "#EF863B",
+      }
+    },
   },
   plugins: [],
 }
