e5ed47eb8445593284cf2a72f5e5d2f243e60482
abilash <abilashnarayanan2001@gmail.com>
2025-02-10 20:41:26 +0530

    Smart search

--- frontend/notes-app/src/components/SearchBar/SearchBar.jsx
+++ frontend/notes-app/src/components/SearchBar/SearchBar.jsx
@@ -1,32 +1,39 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
 
 const SearchBar = ({ value, userInfo, onChange, handleSearch, onClearSearch }) => {
+  useEffect(() => {
+    if (value) {
+      handleSearch();
+    } else {
+      onClearSearch();
+    }
+  }, [value]);
+
   return (
-    userInfo && <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md ">
-      <input
-        type="text"
-        className="w-full text-xs bg-transparent py-[11px] outline-none"
-        value={value}
-        onChange={onChange}
-        placeholder="Search Notes"
-      />
-
-      {value && (
-        <IoMdClose
-          className="text-xl text-slate-500 mr-3 cursor-pointer hover:text-black"
-          onClick={onClearSearch}
+    userInfo && (
+      <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md ">
+        <input
+          type="text"
+          className="w-full text-xs bg-transparent py-[11px] outline-none"
+          value={value}
+          onChange={onChange}
+          placeholder="Search Notes"
         />
-      )}
 
-      <FaMagnifyingGlass
-        className="text-slate-400 cursor-pointer hover:text-black"
-        onClick={handleSearch}
-      />
-    </div>
+        {value && (
+          <IoMdClose
+            className="text-xl text-slate-500 mr-3 cursor-pointer hover:text-black"
+            onClick={onClearSearch}
+          />
+        )}
+
+        <FaMagnifyingGlass className="text-slate-400 cursor-pointer hover:text-black" />
+      </div>
+    )
   );
 };
 
